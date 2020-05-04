17d2fc65bdc510f8f381c9445f622ec151cd32c3
esennesh <elisennesh@gmail.com>
2020-05-04 13:06:52 -0400

    setup.py: remove Github username from package name for easier re/uninstalls
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pytorch-expm-Lezcano", # Replace with your own username
+    name="pytorch-expm", # Replace with your own username
     version="0.0.1",
     author="Mario Lezcano-Casado",
     author_email="mario.lezcanocasado@maths.ox.ac.uk",
